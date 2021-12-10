d0a00102e515bd2483e5be36d8a04d5f228f174b
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-09 22:03:46 -0500

    Componente MyLabel agregado

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,10 @@
+/* Importaciones propias */
+import './myLabel.css';
+
+export const MyLabel = () => {
+    return (
+        <span>
+            Hola Mundo
+        </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,11 @@
+/* Importaciones propias */
+import {MyLabel} from '../../components/MyLabel';
+
+export default {
+    title: 'Example/MyLabel',
+    component: MyLabel
+}
+
+const Template = () => <MyLabel/>
+
+export const Basic = Template.bind({})
