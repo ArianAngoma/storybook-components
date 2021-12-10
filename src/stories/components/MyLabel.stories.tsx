3a4dd3b14677913e04f62903629145899ffd1306
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-10 07:53:33 -0500

    Props y controles agregados a la historia y al componente

--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,11 +1,16 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
 /* Importaciones propias */
 import {MyLabel} from '../../components/MyLabel';
 
 export default {
     title: 'UI/MyLabel',
     component: MyLabel
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/>
+
+export const Basic = Template.bind({});
+Basic.args = {
+    size: 'normal'
 }
-
-const Template = () => <MyLabel/>
-
-export const Basic = Template.bind({})
